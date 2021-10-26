a710b4cf8874cb1566bfe4d8df602acb2fe45b9c
enricop89 <enrico.portolan@gmail.com>
2021-10-26 15:10:20 +0100

    - adding bg images

--- sample-app/virtualbackground/index.js
+++ sample-app/virtualbackground/index.js
@@ -1,7 +1,18 @@
 'use strict';
 
-const { isSupported, BackgroundEffect } = OT.VideoEffects;
+const {
+  isSupported,
+  BackgroundBlurEffect,
+  VirtualBackgroundEffect
+} = OT.VideoEffects;
 const bootstrap = window.bootstrap;
+
+const startBlurEffectButton = document.getElementById('start-blur-effect');
+const startVirtualBgEffectButton = document.getElementById(
+  'start-virtual-bg-effect'
+);
+const stopEffectButton = document.getElementById('stop-effect');
+const backgroundsForm = document.getElementById('backgrounds-form');
 
 // Credentials
 
@@ -16,9 +27,29 @@ const assetsPath = '';
 
 let session = null;
 let publisher = null;
+let backgroundBlur;
 let audioTrack;
 let videoTrack;
 let mediaTrack;
+let virtualBgEffect;
+
+const loadImage = (name) => {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.src = `backgrounds/${name}.jpg`;
+    image.onload = () => resolve(image);
+  });
+};
+
+let backgrounds = {};
+Promise.all([
+  loadImage('vonage_background'),
+  loadImage('simpson_background')
+]).then(([vonageBg, simpsonBg]) => {
+  backgrounds.vonage = vonageBg;
+  backgrounds.simpson = simpsonBg;
+  return backgrounds;
+});
 
 if (!isSupported) {
   errorMessage.textContent = 'This browser is not supported.';
@@ -26,9 +57,8 @@ if (!isSupported) {
 }
 
 const createLocalTrack = () => {
-  return OT.getUserMedia({ resolution: '640x480' })
+  return OT.getUserMedia({ audio: true, video: true })
     .then((track) => {
-      console.log('OTGetUserMedia', track);
       mediaTrack = track;
       audioTrack = track.getAudioTracks()[0];
       console.log('OTGetUserMedia - videoTrack', videoTrack);
@@ -38,19 +68,15 @@ const createLocalTrack = () => {
     });
 };
 
-const init = async () => {
-  await createLocalTrack();
-  const backgroundBlur = new BackgroundEffect({
-    assetsPath: ''
-  });
-  await backgroundBlur.loadModel();
-  const outputVideoStream = backgroundBlur.startEffect(mediaTrack);
-  console.log('outputVideoStream', outputVideoStream);
+const publishToSession = (outputVideoStream) => {
   publisher = OT.initPublisher(
     'publisher',
     {
       audioSource: audioTrack,
-      videoSource: outputVideoStream.getVideoTracks()[0]
+      videoSource: outputVideoStream.getVideoTracks()[0],
+      width: 640,
+      height: 480,
+      insertMode: 'append'
     },
     (err) => {
       console.log('Publisher Created');
@@ -79,4 +105,59 @@ const init = async () => {
   });
 };
 
-init();
+// Start Effect Logic
+
+const initVirtualBgEffect = async () => {
+  const options = {};
+  const inputs = backgroundsForm.elements;
+  for (let item of inputs) {
+    console.log('item', item);
+    item.valueAsNumber
+      ? (options[item.id] = item.valueAsNumber)
+      : (options[item.id] = item.value);
+  }
+  let backgroundImage = backgrounds[options.backgroundImage];
+  if (!virtualBgEffect) {
+    virtualBgEffect = new VirtualBackgroundEffect({
+      assetsPath: '',
+      backgroundImage
+    });
+    await virtualBgEffect.loadModel();
+  } else {
+    virtualBgEffect.backgroundImage = backgroundImage;
+  }
+  await createLocalTrack();
+  await virtualBgEffect.loadModel();
+  publishToSession(virtualBgEffect.startEffect(mediaTrack));
+};
+
+const initBlurEffect = async () => {
+  await createLocalTrack();
+  backgroundBlur = new BackgroundBlurEffect({
+    assetsPath: ''
+  });
+  await backgroundBlur.loadModel();
+  publishToSession(backgroundBlur.startEffect(mediaTrack));
+};
+
+startBlurEffectButton.onclick = (event) => {
+  event.preventDefault();
+  initBlurEffect();
+};
+startVirtualBgEffectButton.onclick = (event) => {
+  event.preventDefault();
+  initVirtualBgEffect();
+};
+
+stopEffectButton.onclick = (event) => {
+  event.preventDefault();
+  if (backgroundBlur) {
+    backgroundBlur.stopEffect();
+  }
+  if (virtualBgEffect) {
+    virtualBgEffect.stopEffect();
+  }
+
+  publisher.destroy();
+  mediaTrack.getTracks().forEach((t) => t.stop());
+};
