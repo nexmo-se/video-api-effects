466013ceb821dbaeaf90859bdceff13efb83b6db
enricop89 <enrico.portolan@gmail.com>
2021-10-27 15:19:16 +0100

    - move index to root

--- sample-app/server.js
+++ sample-app/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-app.use(express.static(resolve(__dirname, './virtualbackground')));
+app.use(express.static(resolve(__dirname, './backgrounds')));
 app.use(express.static(resolve(__dirname, './library')));
 app.use(express.static(resolve(__dirname, '../dist/build')));
+app.use(express.static(resolve(__dirname)));
 
 const port = parseInt(process.env.PORT || '3000');
 app.listen(port, () => {
